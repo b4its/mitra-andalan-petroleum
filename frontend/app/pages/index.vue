5234fe2358800feac2b93f20e4e40b78abbd90f4
N1Coder <tomofanteng123@gmail.com>
2026-07-11 14:36:28 +0800

    fix: restructure side navigation links layout misc: hide chart template chart component on index page misc: add label on quick action button on index page

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,25 +1,30 @@
 <script setup lang="ts">
-import { sub } from 'date-fns'
-import type { DropdownMenuItem } from '@nuxt/ui'
-import type { Period, Range } from '~/types'
+import { sub } from "date-fns";
+import type { DropdownMenuItem } from "@nuxt/ui";
+import type { Period, Range } from "~/types";
 
-const { isNotificationsSlideoverOpen } = useDashboard()
+const { isNotificationsSlideoverOpen } = useDashboard();
 
-const items = [[{
-  label: 'New mail',
-  icon: 'i-lucide-send',
-  to: '/inbox'
-}, {
-  label: 'New customer',
-  icon: 'i-lucide-user-plus',
-  to: '/customers'
-}]] satisfies DropdownMenuItem[][]
+const items = [
+  [
+    {
+      label: "New mail",
+      icon: "i-lucide-send",
+      to: "/inbox",
+    },
+    {
+      label: "New customer",
+      icon: "i-lucide-user-plus",
+      to: "/customers",
+    },
+  ],
+] satisfies DropdownMenuItem[][];
 
 const range = shallowRef<Range>({
   start: sub(new Date(), { days: 14 }),
-  end: new Date()
-})
-const period = ref<Period>('daily')
+  end: new Date(),
+});
+const period = ref<Period>("daily");
 </script>
 
 <template>
@@ -45,7 +50,12 @@ const period = ref<Period>('daily')
           </UTooltip>
 
           <UDropdownMenu :items="items">
-            <UButton icon="i-lucide-plus" size="md" class="rounded-full" />
+            <UButton
+              icon="i-lucide-plus"
+              label="Aksi Cepat"
+              size="md"
+              class="rounded-full"
+            />
           </UDropdownMenu>
         </template>
       </UDashboardNavbar>
@@ -61,8 +71,9 @@ const period = ref<Period>('daily')
     </template>
 
     <template #body>
+      <h1 class="text-5xl font-bold text-neutral-50">Ikhtisar Data</h1>
       <HomeStats :period="period" :range="range" />
-      <HomeChart :period="period" :range="range" />
+      <!-- <HomeChart :period="period" :range="range" /> -->
       <HomeSales :period="period" :range="range" />
     </template>
   </UDashboardPanel>
