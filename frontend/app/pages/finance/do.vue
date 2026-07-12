fc8235cb0540e33ef370fa394891ab36a7d2362d
N1Coder <tomofanteng123@gmail.com>
2026-07-12 15:20:12 +0800

    feat: add structure for finance page

--- frontend/app/pages/finance/do.vue
+++ frontend/app/pages/finance/do.vue
@@ -30,35 +30,11 @@ definePageMeta({ layout: "finance" });
 </script>
 
 <template>
-  <UDashboardPanel id="home">
+  <UDashboardPanel id="delivery-order">
     <template #header>
-      <UDashboardNavbar title="Beranda" :ui="{ right: 'gap-3' }">
+      <UDashboardNavbar title="Delivery Order" :ui="{ right: 'gap-3' }">
         <template #leading>
           <UDashboardSidebarCollapse />
-        </template>
-
-        <template #right>
-          <UTooltip text="Notifications" :shortcuts="['N']">
-            <UButton
-              color="neutral"
-              variant="ghost"
-              square
-              @click="isNotificationsSlideoverOpen = true"
-            >
-              <UChip color="error" inset>
-                <UIcon name="i-lucide-bell" class="size-5 shrink-0" />
-              </UChip>
-            </UButton>
-          </UTooltip>
-
-          <UDropdownMenu :items="items">
-            <UButton
-              icon="i-lucide-plus"
-              label="Aksi Cepat"
-              size="md"
-              class="rounded-full"
-            />
-          </UDropdownMenu>
         </template>
       </UDashboardNavbar>
 
@@ -73,9 +49,8 @@ definePageMeta({ layout: "finance" });
     </template>
 
     <template #body>
-      <h1 class="text-5xl font-bold text-neutral-50">Ikhtisar Data Finance</h1>
-      <FinanceStats :period="period" :range="range" />
-      <!-- <HomeChart :period="period" :range="range" /> -->
+      <h1 class="text-5xl font-bold text-neutral-50">Data Delivery Order</h1>
+
       <FinanceTable :period="period" :range="range" />
     </template>
   </UDashboardPanel>
---
+++ frontend/app/pages/finance/invoice.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const links = [
+  [
+    {
+      label: "Ikhtisar",
+      icon: "i-lucide-square-chart-gantt",
+      to: "/finance/invoice",
+      exact: true,
+    },
+    {
+      label: "Pembuatan Invoice",
+      icon: "i-lucide-receipt",
+      to: "/finance/invoice/pembuatan-invoice",
+    },
+    {
+      label: "Data Invoice Customer",
+      icon: "i-lucide-users",
+      to: "/finance/invoice/data-invoice-customer",
+    },
+  ],
+] satisfies NavigationMenuItem[][];
+
+definePageMeta({ layout: "finance" });
+</script>
+
+<template>
+  <UDashboardPanel id="invoice" :ui="{ body: 'lg:py-12' }">
+    <template #header>
+      <UDashboardNavbar title="Invoice">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+      </UDashboardNavbar>
+
+      <UDashboardToolbar>
+        <!-- NOTE: The `-mx-1` class is used to align with the `DashboardSidebarCollapse` button here. -->
+        <UNavigationMenu :items="links" highlight class="-mx-1 flex-1" />
+      </UDashboardToolbar>
+    </template>
+
+    <template #body>
+      <div
+        class="flex flex-col gap-4 sm:gap-6 lg:gap-12 w-full lg:max-w-7xl mx-auto"
+      >
+        <NuxtPage />
+      </div>
+    </template>
+  </UDashboardPanel>
+</template>
