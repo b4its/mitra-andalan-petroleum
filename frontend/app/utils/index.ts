e8a409f22194a88017b00e9b9c84bf570fed1b85
N1Coder <tomofanteng123@gmail.com>
2026-07-15 15:46:58 +0800

    feat: add types for offering letter data feat: add formatcurrency util

--- frontend/app/utils/index.ts
+++ frontend/app/utils/index.ts
@@ -1,7 +1,25 @@
 export function randomInt(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export function randomFrom<T>(array: T[]): T {
-  return array[Math.floor(Math.random() * array.length)]!
+  return array[Math.floor(Math.random() * array.length)]!;
+}
+
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    currencyDisplay: "narrowSymbol",
+  }).format(value);
+}
+
+export function formatDate(date: string | Date) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  };
+
+  return new Intl.DateTimeFormat("id-ID", options).format(new Date(date));
 }
