c529ef76dd0359cb108ddae8d2b1fd490518ee99
N1Coder <tomofanteng123@gmail.com>
2026-07-15 15:43:56 +0800

    fix: make the content full width

--- frontend/app/pages/marketing/customer.vue
+++ frontend/app/pages/marketing/customer.vue
@@ -41,9 +41,7 @@ definePageMeta({ layout: "marketing" });
     </template>
 
     <template #body>
-      <div
-        class="flex flex-col gap-4 sm:gap-6 lg:gap-12 w-full lg:max-w-7xl mx-auto"
-      >
+      <div class="flex flex-col gap-4 sm:gap-6 lg:gap-12 w-full">
         <NuxtPage />
       </div>
     </template>
