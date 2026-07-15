d26d0ac5032772b9ec3c6d52f3326f616efc078e
N1Coder <tomofanteng123@gmail.com>
2026-07-15 15:44:34 +0800

    misc: remove unused props

--- frontend/app/pages/marketing/supplier/index.vue
+++ frontend/app/pages/marketing/supplier/index.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
-import { sub } from "date-fns";
-import type { Period, Range } from "~/types";
-
-const range = shallowRef<Range>({
-  start: sub(new Date(), { days: 14 }),
-  end: new Date(),
-});
-const period = ref<Period>("daily");
-
 definePageMeta({ layout: "marketing" });
 </script>
 
 <template>
-  <h1 class="text-5xl font-bold text-neutral-50">Rekap Histori PO Supplier</h1>
-  <HomeSales :period="period" :range="range" />
+  <h1 class="text-3xl font-bold text-neutral-50">
+    Rekap Histori Purchase Order Supplier
+  </h1>
+  <MarketingPOSupplierData />
 </template>
