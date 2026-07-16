49dfe8c390f6de88b0748fe8104e6f095b966c78
N1Coder <tomofanteng123@gmail.com>
2026-07-16 22:51:36 +0800

    feat: init invoice form components feat: init invoice form page feat: create invoice form schema

---
+++ frontend/app/components/finance/invoice/InvoiceDetailsForm.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import type { FormSubmitEvent } from "@nuxt/ui";
+import {
+  financeInvoiceDetailsSchema,
+  type FinanceInvoiceDetailsState,
+} from "~/types/schemas";
+
+defineProps<{
+  hasPrevious: boolean | undefined;
+}>();
+
+const emit = defineEmits<{
+  submit: [];
+  previous: [];
+}>();
+
+const state = defineModel<FinanceInvoiceDetailsState>({ required: true });
+
+function previous() {
+  emit("previous");
+}
+
+function onSubmit(_event: FormSubmitEvent<FinanceInvoiceDetailsState>) {
+  emit("submit");
+}
+</script>
+
+<template>
+  <UForm
+    id="invoice-header"
+    :schema="financeInvoiceDetailsSchema"
+    :state="state"
+    :ui="{ base: 'lg:w-full lg:max-w-2xl lg:mx-auto mt-8' }"
+    @submit="onSubmit"
+  >
+    <UPageCard variant="soft">
+      <p>Heading</p>
+
+      <USeparator />
+
+      <div class="flex justify-between pt-4">
+        <UButton
+          variant="ghost"
+          color="neutral"
+          leading-icon="i-lucide-arrow-left"
+          :disabled="!hasPrevious"
+          @click="previous"
+        >
+          Sebelumnya
+        </UButton>
+
+        <UButton type="submit" trailing-icon="i-lucide-arrow-right">
+          Selanjutnya
+        </UButton>
+      </div>
+    </UPageCard>
+  </UForm>
+</template>
